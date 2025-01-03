539e594ecd6887dc6ca15f732fe784a299828813
Satyam Srivastava <srivastava6808@gmail.com>
2025-01-04 00:24:06 +0530

    Created Customer jsx file

---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,10 @@
+function Chai(){
+    return(
+        <h2>chai is ready using vite</h2>
+    )
+}
+
+export default Chai;
+
+// File name should be jsx 
+// Function Name should be in Sentence Casing
