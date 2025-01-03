07a07a28d07770b70cf3cd45b6c93657b8a4c44f
Satyam Srivastava <srivastava6808@gmail.com>
2025-01-04 00:58:04 +0530

    Workflow Custom Element

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,11 @@
-
+import Chai from "./chai"
 function App() {
+  const reactBundle = "Vite"
   return (
-    <h1>Hello World ! Chai aur code with Vite</h1>
+    <>
+    <h1>Hello World ! Chai aur code with {reactBundle}</h1> {/*{} is called evaluated expression. means it shows the outcome of the js code */}
+    {/* <Chai/> */}
+    </>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,44 @@
-import { StrictMode } from 'react'
+import React from 'react';
+
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+// creating element using react fuctions
+const anotherUser = "Checking Variable inside DOM "
+const reactElement = React.createElement(
+  'a',
+  {href:'https://google.com',
+    target:"_blank"
+  },
+  "Click to visit Google",
+  anotherUser // when dom tree is created then Variables are added in the tree also called evaluatted Expression
+)
+
+const anotherElement =(
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+)
+const reactCustomElement ={
+  type:'a',
+  props:{
+      href : 'https://google.com',
+      target :'_blank'
+  },
+  children :'Click me to visit Google'
+}
+
+function MyApp (){
+  return(
+    <div>
+    <h1>This Is My  custom App</h1> 
+    </div>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // reactElement  //Created using the react Function 
+  // anotherElement //visible a it is easily converted to the DOM supported by the React i.e it is converted to the tree like structure
+  // reactCustomElement  //not visible as it is not rendred correctly as react requires its syntax for rendring elements
+  <App/>
+  // MyApp()
 )
