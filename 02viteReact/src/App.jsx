56f86c47a3222922fad2c8656a23f60c6b895085
Satyam Srivastava <srivastava6808@gmail.com>
2025-01-06 18:31:44 +0530

    Hooks in react

---
+++ 02viteReact/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  // let counter = 5
+  let [counter,setcounter] = useState(5) // first is variable name second one is funtion that performs on the variable 
+
+
+  const addValue =() =>{
+    // console.log("Value Added",Math.random());
+    console.log("Incremented Counter ",counter  )
+    if(counter<20)
+      counter = counter+1
+    setcounter(counter)
+  }
+  const removeValue=()=>{
+    console.log("Decremented Counter", counter)
+    if(counter>0)
+      counter=counter-1
+    setcounter(counter)
+  }
+
+  return (
+    <>
+    <h1>Chai or React Counter App </h1>
+    <h2>Counter value: {counter}</h2>
+
+    <button onClick={
+      addValue
+    } disabled={counter===20}>Add Value</button>
+    <br/>
+    <button onClick={
+      removeValue
+    } disabled={counter===0}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
