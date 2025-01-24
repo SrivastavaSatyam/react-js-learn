e377d277acd61a3c893e570819a98cc62cc4f9e9
Satyam Srivastava <srivastava6808@gmail.com>
2025-01-24 18:22:20 +0530

    Color Changer (context Api)

---
+++ 09themeSwitcher_ContextApi/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode:"class", // added bcz of html 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
